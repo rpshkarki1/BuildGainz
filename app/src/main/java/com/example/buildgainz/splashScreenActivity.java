20ab6e05c55b6314d2537095521b8c7056fc00c7
rpshkarki1 <rpshkarki@gmail.com>
2023-06-26 20:16:56 +0545

    Intent passed Splash Screen to getStarted Screen

---
+++ app/src/main/java/com/example/buildgainz/splashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.buildgainz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Intent getStarted = new Intent(splashScreenActivity.this,getStarted.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(getStarted);
+                finish();
+            }
+        },3000);
+    }
+}
