6151196a523338d122c83014a472a2d3d1f4813d
XXMORTIS <rpshkarki@gmail.com>
2023-08-12 21:17:55 -0700

    Fixed activity_get_started.xml

--- app/src/main/java/com/example/buildgainz/SplashScreenActivity.java
+++ app/src/main/java/com/example/buildgainz/SplashScreenActivity.java
@@ -2,25 +2,23 @@ package com.example.buildgainz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class splashScreenActivity extends AppCompatActivity {
+@SuppressLint("CustomSplashScreen")
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Intent getStarted = new Intent(SplashScreenActivity.this, getStartedActivity.class);
 
-        Intent getStarted = new Intent(splashScreenActivity.this, getStartedActivity.class);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(getStarted);
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            startActivity(getStarted);
+            finish();
         },3000);
     }
 }
