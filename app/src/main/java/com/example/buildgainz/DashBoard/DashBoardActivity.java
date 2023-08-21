177e6d4a740a731b8f8b41a36fa67313d7272eab
XXMORTIS <rpshkarki@gmail.com>
2023-08-21 10:11:55 +0545

    Saving up

--- app/src/main/java/com/example/buildgainz/DashBoard/DashBoardActivity.java
+++ app/src/main/java/com/example/buildgainz/DashBoard/DashBoardActivity.java
@@ -6,6 +6,7 @@ import static com.example.buildgainz.R.menu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -13,9 +14,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
 
+import com.example.buildgainz.DashBoard.Profile.ProfileActivity;
 import com.example.buildgainz.DashBoard.Settings.ChangePasswordActivity;
 import com.example.buildgainz.DashBoard.Settings.DeleteUserActivity;
-import com.example.buildgainz.DashBoard.Profile.ProfileActivity;
 import com.example.buildgainz.LoginPage.LoginPageActivity;
 import com.example.buildgainz.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,73 +24,78 @@ import com.google.firebase.auth.FirebaseAuth;
 public class DashBoardActivity extends AppCompatActivity {
 
     Toolbar toolbar;
-    ImageButton imageButton, profileButton;
+    ImageButton settingButton, profileButton;
+    RelativeLayout exercisePlan;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(layout.activity_dash_board);
+    protected void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( layout.activity_dash_board );
 
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        toolbar = findViewById ( R.id.toolbar );
+        setSupportActionBar ( toolbar );
 
-        profileButton = findViewById(R.id.profileBtn);
-        profileButton.setOnClickListener(v -> {
+        settingButton = findViewById ( R.id.settingBtn );
+        profileButton = findViewById ( R.id.profileBtn );
+        exercisePlan = findViewById ( R.id.exerCardView );
+
+        profileButton.setOnClickListener ( v -> {
 
 
-                Intent intent = new Intent(DashBoardActivity.this, ProfileActivity.class);
+            Intent intent = new Intent ( DashBoardActivity.this , ProfileActivity.class );
 
-                startActivity(intent);
+            startActivity ( intent );
 
-        });
+        } );
 
-        imageButton = findViewById(R.id.settingBtn);
-        imageButton.setOnClickListener(v -> {
-            PopupMenu popupMenu = new PopupMenu(DashBoardActivity.this, imageButton);
-            popupMenu.getMenuInflater().inflate(menu.setting_menu, popupMenu.getMenu());
-            popupMenu.show();
+        settingButton.setOnClickListener ( v -> {
+            PopupMenu popupMenu = new PopupMenu ( DashBoardActivity.this , settingButton );
+            popupMenu.getMenuInflater ( ).inflate ( menu.setting_menu , popupMenu.getMenu ( ) );
+            popupMenu.show ( );
 
-            popupMenu.setOnMenuItemClickListener(item -> {
+            popupMenu.setOnMenuItemClickListener ( item -> {
 
-                int id = item.getItemId();
+                int id = item.getItemId ( );
 
-                if (id == R.id.changePass) {
-                    startActivity(new Intent(DashBoardActivity.this, ChangePasswordActivity.class));
-                }else if (id == R.id.deleteAcc) {
-                    startActivity(new Intent(DashBoardActivity.this, DeleteUserActivity.class));
-                }else if (id == R.id.logout) {//create logout with DialogBox
-                    showLogoutDialog();
+                if ( id == R.id.changePass ) {
+                    startActivity ( new Intent ( DashBoardActivity.this , ChangePasswordActivity.class ) );
+                } else if ( id == R.id.deleteAcc ) {
+                    startActivity ( new Intent ( DashBoardActivity.this , DeleteUserActivity.class ) );
+                } else if ( id == R.id.logout ) {//create logout with DialogBox
+                    showLogoutDialog ( );
                     return true;
                 } else {
-                    Toast.makeText(DashBoardActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    Toast.makeText ( DashBoardActivity.this , "Something went wrong" , Toast.LENGTH_SHORT ).show ( );
                 }
-                return DashBoardActivity.super.onOptionsItemSelected(item);
-            });
-        });
+                return DashBoardActivity.super.onOptionsItemSelected ( item );
+            } );
+        } );
+
+        exercisePlan.setOnClickListener ( v -> startActivity ( new Intent ( DashBoardActivity.this, Exercises.class ) ) );
+
     }
 
-    private void showLogoutDialog() {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setTitle("Logout");
-        alertDialogBuilder.setMessage("Are you sure you want to logout?");
-        alertDialogBuilder.setPositiveButton("Yes", (dialogInterface, i) -> {
-            FirebaseAuth.getInstance().signOut();
-            Intent intent = new Intent(DashBoardActivity.this, LoginPageActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-            finish();
-        });
-        alertDialogBuilder.setNegativeButton("No", (dialogInterface, i) -> {
+    private void showLogoutDialog ( ) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder ( this );
+        alertDialogBuilder.setTitle ( "Logout" );
+        alertDialogBuilder.setMessage ( "Are you sure you want to logout?" );
+        alertDialogBuilder.setPositiveButton ( "Yes" , ( dialogInterface , i ) -> {
+            FirebaseAuth.getInstance ( ).signOut ( );
+            Intent intent = new Intent ( DashBoardActivity.this , LoginPageActivity.class );
+            intent.addFlags ( Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK );
+            startActivity ( intent );
+            finish ( );
+        } );
+        alertDialogBuilder.setNegativeButton ( "No" , ( dialogInterface , i ) -> {
             // Do nothing or handle cancellation if needed.
-        });
+        } );
 
 
+        AlertDialog alertDialog = alertDialogBuilder.create ( );
 
-        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.setOnShowListener ( dialog -> alertDialog.getButton ( AlertDialog.BUTTON_POSITIVE ).setTextColor ( getResources ( ).getColor ( R.color.red ) ) );
 
-        alertDialog.setOnShowListener(dialog -> alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.red)));
-
-        alertDialog.show();
+        alertDialog.show ( );
     }
 }
 
---
+++ app/src/main/java/com/example/buildgainz/DashBoard/Exercises.java
@@ -0,0 +1,16 @@
+package com.example.buildgainz.DashBoard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.buildgainz.R;
+
+public class Exercises extends AppCompatActivity {
+
+    @Override
+    protected void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_exercises );
+    }
+}
